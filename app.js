a4082ec3f39a2e20fa298c683a914d63cfbed4bd
likhity <73198499+likhity@users.noreply.github.com>
2021-12-24 16:44:59 -0700

    Setup basic server

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+
+// set view engine to ejs
+// it means that all of our views are going to be written in ejs
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI = "mongodb://localhost/switzerlandappdb";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, console.log(`Server listening on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
